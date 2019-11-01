216dcb75877f9ec29decc9a869119656cc14022e
Daniel Chukwurah <dnchukwurah@sidmach.com>
2019-11-01 11:00:55 +0100

    bootstrap installed and required folders created

---
+++ src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <nav-bar />
+        <footer />
+    </div>
+</template>
+
+<script>
+import NavBar from '../components/HomePage/NavBar.vue';
+import NavBar from '../components/HomePage/Footer.vue';
+export default {
+    components: {
+        'nav-bar': NavBar,
+        'footer': Footer
+    }
+}
+</script>
