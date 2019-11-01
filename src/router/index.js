ae7e1cd1b387aad5dfa998b7042251c6dc0167b7
Daniel Chukwurah <dnchukwurah@sidmach.com>
2019-11-01 11:39:15 +0100

    router for main menu added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import HomePage from '../views/HomePage.vue'
+import Services from '../views/Services.vue'
+import LogIn from '../components/LogIn.vue'
+import SignUp from '../components/SignUp.vue'
+import JoinAsPro from '../components/JoinAsPro.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +12,32 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: HomePage
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/services',
+    name: 'services',
+    component: Services
+  },
+  {
+    path: '/store',
+    name: 'store',
+    component: Store
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignUp
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LogIn
+  }
+  {
+    path: '/join',
+    name: 'join',
+    component: JoinAsPro
   }
 ]
 
