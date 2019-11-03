a601eb010fbde1f7166ac5443b676bcd503a3523
Daniel Chukwurah <dnchukwurah@sidmach.com>
2019-11-03 20:15:07 +0100

    new changes

---
+++ src/components/HomePage/TheFooter.vue
@@ -0,0 +1,97 @@
+<template>
+    <div>
+        <footer>
+            <div class="container pt-5 border-bottom">
+                <div class="row">
+
+                     <div class="col-md-3 col-sm-12 mb-3">
+                        <h5 class="mb-4 font-weight-bold">iGofer</h5>
+                        <ul class="list-group">
+                            <li class="list-group-item  border-0 p-0 mb-2"><a href="#">About</a></li>
+                            <li class="list-group-item  border-0 p-0 mb-2"><a href="#">Blog</a></li>
+                            <li class="list-group-item  border-0 p-0 mb-2"><a href="#">Careers</a></li>
+                            <li class="list-group-item  border-0 p-0 mb-2"><a href="#">Press</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-9 col-sm-12">
+
+                    <div class="col-md-3 col-sm-6 col-6 p-0 float-left mb-3">
+                        <h5 class="mb-4 font-weight-bold">Popular Services</h5>
+                        <ul class="list-group">
+                            <li class="list-group-item  border-0 p-0 mb-2"><a href="#">Carpentry</a></li>
+                            <li class="list-group-item  border-0 p-0 mb-2"><a href="#">Cleaning</a></li>
+                            <li class="list-group-item  border-0 p-0 mb-2"><a href="#">Fumigation</a></li>
+                            <li class="list-group-item  border-0 p-0 mb-2"><a href="#">Installations</a></li>
+                            <li class="list-group-item  border-0 p-0 mb-2"><a href="#">Painting</a></li>
+                            <li class="list-group-item  border-0 p-0 mb-2"><a href="#">Plumbering</a></li>
+                        </ul>
+                    </div>
+
+
+                        <div class="col-md-3 col-sm-6 col-6 p-0 float-left mb-3">
+                            <h5 class="mb-4 font-weight-bold">Support</h5>
+                            <ul class="list-group">
+                                <li class="list-group-item border-0 p-0 mb-2"><a href="#">Help</a></li>
+                                <li class="list-group-item border-0 p-0 mb-2"><a href="#"> Privacy policy</a></li>
+                                <li class="list-group-item border-0 p-0 mb-2"><a href="#">Terms and conditions</a></li>
+                            </ul>
+                        </div>
+
+
+
+                        <div class="col-md-3 col-sm-6 col-6 mb-3 p-0 float-left">
+                            <h5 class="mb-4 font-weight-bold">Connect</h5>
+                                    <a href="https://www.facebook.com/igoferNG" target="_blank" class="p-1">  <i class="ti-facebook"></i></a>
+                                    <a href="https://twitter.com/igoferNG" target="_blank" class="p-1">  <i class="ti-twitter-alt"></i></a>
+                                    <!-- <a href="https://linkedin.com" target="_blank" class="p-1"><i class="ti-linkedin"></i></a> -->
+                                    <a href="https://www.instagram.com/igoferng/" target="_blank" class="p-1"><i class="ti-instagram"></i></a>
+                                    <a href="https://www.youtube.com" target="_blank" class="p-1"><i class="ti-youtube"></i></a>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-6 p-0 mb-3 float-left">
+                            <h5 class="mb-4 font-weight-bold">Download the app</h5>
+                            <img src="assets/img/playstore.svg" alt="playstore">
+                        </div>
+
+                    </div>
+                </div>
+
+                 <hr>
+                    <div>
+                        <p class="text-center">&copy; 2019 iGofer. All rights reserved.</p>
+                    </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+    footer {
+        background: #F5F5F7 !important;
+    }
+
+    .list-group-item{
+        background-color: #F5F5F7 !important;
+    }
+
+
+    h5 {
+        font-size: 1rem;
+        line-height: 1.375;
+        font-family: "Rubik", "Arial", "sans-serif";
+        font-weight: 500;
+        color: #333456 !important;
+    }
+
+    a {
+        color: #333456 !important;
+        font-weight: 500 !important;
+        text-decoration: none !important;
+    }
+
+    a:hover {
+        color: #007bff;
+        text-decoration: none;
+    }
+</style>
